a8d219eceb8ce8cdb2ff547134d5270195bacc26
Replit user <replituser@example.com>
2021-03-26 21:42:23 +0000

    chore: adiciona tratamento de entrada do input

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ const reader = readline.createInterface({
 });
 
 
-reader.question("Qual a Posição Inicial do Cavalo?\n", function(answer) {
-      
+reader.question("Qual a Posição Inicial do Cavalo?\n", async function(answer) {
+    const [horizontalLetter,vertical,rest] = answer.slice('');
+     const valid =  await board.setHorseStartingPosition(horizontalLetter,parseInt(vertical));
+    if(!valid){
+      console.log("Finalizando Aplicação...");
+      return;
+    }
 
+    board.walkTheHorse();
     reader.close();
 });
