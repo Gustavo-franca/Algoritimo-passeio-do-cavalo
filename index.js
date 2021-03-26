060f274ab773791752f128d6ac30777890ea49b9
Replit user <replituser@example.com>
2021-03-26 21:01:11 +0000

    chore: inicia o tabuleiro primeiro

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import readline from 'readline';
+import createChessBoad  from './chessBoard.js';
 
+const board = createChessBoad();
 const reader = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -11,4 +13,3 @@ reader.question("Qual a Posição Inicial do Cavalo?\n", function(answer) {
 
     reader.close();
 });
-
