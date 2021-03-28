e303e833f989a234847a2dad205f768e571dea42
Replit user <replituser@example.com>
2021-03-28 00:18:14 +0000

    feat: adiciona suporte a entrada por argumentos e por readline

--- index.js
+++ index.js
@@ -2,22 +2,37 @@ import readline from 'readline';
 import createChessBoad  from './chessBoard.js';
 import createHorse from './Horse.js'
 
-const board = createChessBoad();
-const reader = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+function main(horizontalLetter,verticalNumber){
+  try{
+      const board = createChessBoad();
+      const horse = createHorse(board);
+      board.addHorse(horse,parseInt(verticalNumber), horizontalLetter);
+      board.walkTheHorse();
+  }catch(err){
+    console.log(err.message());
+  }
+   
+}
 
-reader.question("Qual a Posição Inicial do Cavalo?\n", function(answer) {
 
-    const [horizontalLetter,vertical,rest] = answer.slice('');
-    reader.pause();
-     const horse = createHorse(board);
-     const valid = board.addHorse(horse,parseInt(vertical),horizontalLetter);
-    if(!valid){
-      console.log("Finalizando Aplicação...");
-      return;
-    }
-    board.walkTheHorse();
- 
-});
+const  args = process.argv.slice(2,
+process.argv.length -1
+);
+const input = args[0];
+if(input){
+  const [horizontalLetter,vertical,rest] = input.slice('');
+  main(horizontalLetter,vertical);
+}else{
+  const reader = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+  });
+
+  reader.question("Qual a Posição Inicial do Cavalo?\n", function(answer) {
+      const [horizontalLetter,vertical,rest] = answer.slice('');
+      reader.pause();
+      main(horizontalLetter,vertical);
+  });
+
+}
+
